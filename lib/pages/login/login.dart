dac351f395ed4de0237fa2cb76852de4606b5c97
huanggengzhong <915835528@qq.com>
2020-06-02 12:54:28 +0800

    登录页面

--- lib/pages/login/login.dart
+++ lib/pages/login/login.dart
@@ -1,12 +1,42 @@
 import 'package:flutter/material.dart';
-import 'package:goodhouse/widgets/page_content.dart';
+// import 'package:goodhouse/widgets/page_content.dart';
 class LoginPage extends StatelessWidget {
   const LoginPage({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: PageContent(name:"登录页"),
+    return Scaffold(
+      appBar:AppBar(
+        title:Text("登录")
+      ),
+      body:Column(
+        children: <Widget>[
+          TextField(//文本输入框组件
+          decoration:InputDecoration(
+            labelText:'用户名',
+            hintText:'请输入用户名'
+          ),
+          ),
+          TextField(
+            decoration: InputDecoration(
+              labelText:"密码",
+              hintText:"请输入密码"
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text("还没有账号,"),
+              FlatButton(
+                child: Text("去注册"),
+                onPressed: (){
+                Navigator.pushNamed(context, 'register');
+              })
+            ],
+          )
+          
+        ]
+      )
     );
   }
 }
