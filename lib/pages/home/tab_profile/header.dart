12aac30c08debf99472f8f82b1fed0c27e2176db
huanggengzhong <huanggengzhong@qq.com>
2020-06-18 22:44:58 +0800

    上传

--- lib/pages/home/tab_profile/header.dart
+++ lib/pages/home/tab_profile/header.dart
@@ -4,11 +4,8 @@ var loginFontStyle = TextStyle(fontSize: 20.0, color: Colors.white);
 
 class Header extends StatelessWidget {
   const Header({Key key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    var isLogin=false;
-    return Container(
+Widget _noLoginPageHeader(BuildContext context){
+   return Container(
       decoration: BoxDecoration(color: Colors.green),
       height: 95.0,
       padding: EdgeInsets.only(top: 10.0, left: 20.0, bottom: 20.0),
@@ -52,5 +49,45 @@ class Header extends StatelessWidget {
         )
       ]),
     );
+}
+Widget _loginPageHeader(BuildContext context){
+  String userName="张三";
+  String userImage="https://tva1.sinaimg.cn/large/006y8mN6ly1g6tbnovh8jj30hr0hrq3l.jpg";
+   return Container(
+      decoration: BoxDecoration(color: Colors.green),
+      height: 95.0,
+      padding: EdgeInsets.only(top: 10.0, left: 20.0, bottom: 20.0),
+      child: Row(children: <Widget>[
+        Container(
+          height: 65.0,
+          width: 65.0,
+          margin: EdgeInsets.only(right: 15.0),
+          child: CircleAvatar(
+            //圆形头像
+            backgroundImage: NetworkImage(userImage),
+          ),
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Padding(padding: EdgeInsets.only(top: 6.0)),
+            Row(
+              children: <Widget>[
+               
+                Text(userName, style: loginFontStyle),
+               
+              ],
+            ),
+            Text('查看编辑个人资料',style:TextStyle(color:Colors.white))
+          ],
+        )
+      ]),
+    );
+}
+
+  @override
+  Widget build(BuildContext context) {
+    var isLogin=false;
+   return isLogin ?_noLoginPageHeader(context):_loginPageHeader(context);
   }
 }
