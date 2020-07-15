ec1a3830ecc305dfda2f9d8e8551994bc87ff1f9
huanggengzhong <915835528@qq.com>
2020-07-15 23:06:07 +0800

    备份

--- lib/scoped_model/room_filter.dart
+++ lib/scoped_model/room_filter.dart
@@ -1,40 +1,44 @@
+import 'package:goodhouse/pages/home/tab_search/filter_bar/data.dart';
 import 'package:scoped_model/scoped_model.dart';
 
-class FilterBarModel extends Model{
-  List<GeneralType> _roomTypeList=[];
-  List<GeneralType> _orienteList=[];
-  List<GeneralType> _floorList=[];
+class FilterBarModel extends Model {
+  List<GeneralType> _roomTypeList = [];
+  List<GeneralType> _orientedList = [];
+  List<GeneralType> _floorList = [];
 
-  Set<String> _selectedList =Set<String>();
+  Set<String> _selectedList = Set<String>();
   // 读数据
-  Map<String,List<GeneralType>> get dataList{
-    var result= Map<String,List<GeneralType>>();
-    result['roomTypeList']=_roomTypeList;
-    result['orientedList']=_orienteList;
-    result['floorList']=_floorList;
+  Map<String, List<GeneralType>> get dataList {
+    var result = Map<String, List<GeneralType>>();
+    result['roomTypeList'] = _roomTypeList;
+    result['orientedList'] = _orientedList;
+    result['floorList'] = _floorList;
     return result;
   }
-  // 写数据
-set dataList(Map<String,List<GeneralType>>data) {
-  _roomTypeList=data['roomTypeList'];
-  _orientedList=data['orientedList'];
-  _floorList=data['floorList'];
-  notifyListeners();
-}
+
+  get selectedList => null;
+
+  // 写数据方法
+  set dataList(Map<String, List<GeneralType>> data) {
+    _roomTypeList = data['roomTypeList'];
+    _orientedList = data['orientedList'];
+    _floorList = data['floorList'];
+    notifyListeners();
+  }
 
 // _selectedList 对应的函数方法
 // 读取数据
-Set<String> getSelectedList{
-  return _selectedList;
-}
+  Set<String> get getSelectedList {
+    return _selectedList;
+  }
 
 // 设置数据
-selectedListToggleItem(String data){
-  if(_selectedList.contains(data)){
-    _selectedList.remove(data);
-  }else{
-    _selectedList.add(data);
+  selectedListToggleItem(String data) {
+    if (_selectedList.contains(data)) {
+      _selectedList.remove(data);
+    } else {
+      _selectedList.add(data);
+    }
+    notifyListeners();
   }
 }
-}
-
