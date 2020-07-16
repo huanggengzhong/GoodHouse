d6d8785c145a7f1d6e31cb3ff8462cea97023945
huanggengzhong <915835528@qq.com>
2020-07-17 00:19:25 +0800

    请求

--- README.md
+++ README.md
@@ -1697,3 +1697,58 @@ class _FilterBarState extends State<FilterBar> {
 }
 
 ```
+### 接口联调
+
+#### 封装dio请求
+文件utils/dio_http.dart
+核心代码
+```js
+import 'dart:io';
+
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import '../config.dart';
+
+class DioHttp {
+  Dio _client;
+  BuildContext context;
+
+  static DioHttp of(BuildContext context) {
+    return DioHttp.internal(context);
+  }
+
+  DioHttp.internal(BuildContext context) {
+    if (_client != null || context != this.context) {
+      this.context = context;
+      var options = BaseOptions(
+          baseUrl: Config.BaseUrl,
+          connectTimeout: 1000 * 10,
+          receiveTimeout: 1000 * 3,
+          extra: {'context': context});
+      var client = Dio(options);
+      this._client = client;
+    }
+  }
+
+  Future<Response<Map<String, dynamic>>> get(String path,
+      [Map<String, dynamic> params, String token]) async {
+    var options = Options(headers: {'Authorization': token});
+    return await _client.get(path, queryParameters: params, options: options);
+  }
+
+  Future<Response<Map<String, dynamic>>> post(String path,
+      [Map<String, dynamic> params, String token]) async {
+    var options = Options(headers: {'Authorization': token});
+    return await _client.post(path, data: params, options: options);
+  }
+
+  Future<Response<Map<String, dynamic>>> postFormData(String path,
+      [Map<String, dynamic> params, String token]) async {
+    var options = Options(
+        contentType: ContentType.parse('multipart/form-data'),
+        headers: {'Authorization': token});
+    return await _client.post(path, data: params, options: options);
+  }
+}
+
+```
--- lib/pages/register.dart
+++ lib/pages/register.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:goodhouse/utils/common_toast.dart';
+import 'package:goodhouse/utils/dio_http.dart';
+import 'package:goodhouse/utils/string_is_null_or_empty.dart';
 
 // import 'package:goodhouse/widgets/page_content.dart';
 class RigisterPage extends StatefulWidget {
@@ -11,6 +15,36 @@ class RigisterPage extends StatefulWidget {
 }
 
 class RigisterPageState extends State<RigisterPage> {
+  var usernameController = TextEditingController();
+  var passwordController = TextEditingController();
+  var repeatPasswordController = TextEditingController();
+
+  // 注册方法
+  _registerHandler() async {
+    var username = usernameController.text;
+    var password = passwordController.text;
+    var repeatPassword = repeatPasswordController.text;
+    if (password != repeatPassword) {
+      CommontToast.showToast('两次输入密码不一致!');
+      return;
+    }
+    if(stringIsNullOrEmpty(username)||stringIsNullOrEmpty(password)) {
+      CommontToast.showToast("用户名或者密码不能为空");
+      return;
+    }
+    const url="/register";
+    var params ={'username':username,'password':password};
+
+    var res=await DioHttp.of(context).post(url,params);
+    var resString=json.decode(res.toString());
+    print(resString);
+    print(resString.data.code.contains('0'));
+    if(resString.data.code.contains('0')){
+      CommontToast.showToast("登录成功!");
+    }
+
+  }
+
   // 定义一个bool值
   bool showPassword = false;
   @override
@@ -21,6 +55,7 @@ class RigisterPageState extends State<RigisterPage> {
           minimum: EdgeInsets.all(30.0),
           child: ListView(children: <Widget>[
             TextField(
+              controller: usernameController,
               //文本输入框组件
               decoration: InputDecoration(labelText: '用户名', hintText: '请输入用户名'),
             ),
@@ -28,39 +63,41 @@ class RigisterPageState extends State<RigisterPage> {
               padding: EdgeInsets.all(10.0),
             ),
             TextField(
+              controller: passwordController,
               decoration: InputDecoration(
-                  labelText: "密码",
-                  hintText: "请输入密码",
-                  // 注册页面不需要
-                  // suffixIcon: IconButton(
-                  //     icon: Icon(showPassword
-                  //         ? Icons.visibility_off
-                  //         : Icons.visibility),
-                  //     onPressed: () {
-                  //       setState(() {
-                  //         showPassword = !showPassword;
-                  //       });
-                  //     }) //使用文本框 TextField 自带的属性【后缀图标 suffixIcon】
-                  ),
+                labelText: "密码",
+                hintText: "请输入密码",
+                // 注册页面不需要
+                // suffixIcon: IconButton(
+                //     icon: Icon(showPassword
+                //         ? Icons.visibility_off
+                //         : Icons.visibility),
+                //     onPressed: () {
+                //       setState(() {
+                //         showPassword = !showPassword;
+                //       });
+                //     }) //使用文本框 TextField 自带的属性【后缀图标 suffixIcon】
+              ),
             ),
             Padding(
               padding: EdgeInsets.all(10.0),
             ),
             TextField(
+              controller: repeatPasswordController,
               decoration: InputDecoration(
-                  labelText: "确认密码",
-                  hintText: "请再次输入密码",
-                  // 注册页面不需要
-                  // suffixIcon: IconButton(
-                  //     icon: Icon(showPassword
-                  //         ? Icons.visibility_off
-                  //         : Icons.visibility),
-                  //     onPressed: () {
-                  //       setState(() {
-                  //         showPassword = !showPassword;
-                  //       });
-                  //     }) //使用文本框 TextField 自带的属性【后缀图标 suffixIcon】
-                  ),
+                labelText: "确认密码",
+                hintText: "请再次输入密码",
+                // 注册页面不需要
+                // suffixIcon: IconButton(
+                //     icon: Icon(showPassword
+                //         ? Icons.visibility_off
+                //         : Icons.visibility),
+                //     onPressed: () {
+                //       setState(() {
+                //         showPassword = !showPassword;
+                //       });
+                //     }) //使用文本框 TextField 自带的属性【后缀图标 suffixIcon】
+              ),
             ),
             Padding(
               padding: EdgeInsets.all(10.0),
@@ -68,11 +105,12 @@ class RigisterPageState extends State<RigisterPage> {
             RaisedButton(
               color: Colors.green,
               child: Text(
-                "登录",
+                "注册",
                 style: TextStyle(color: Colors.white),
               ),
               onPressed: () {
-                print("登录");
+                // print("注册");
+                _registerHandler();
               },
             ),
             Padding(
